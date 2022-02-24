5b58efa8de0ef2bdf87976744de3911fc54de9e6
refreshing-lemonade <75630326+refreshing-lemonade@users.noreply.github.com>
2022-02-23 20:10:28 -0800

    Add files via upload

---
+++ global/header.js
@@ -0,0 +1,12 @@
+//    Home | Projects | RUT | Extra | About
+
+document.write("<div style='display:flex; float:right;'>");
+    document.write("<img src='https://refreshinglemona.de/global/zap.png' alt='Home' style='height:90px;'>");
+    document.write("<div id='headerbox'>");
+        document.write("<a class='headerstyle' href='https://refreshinglemona.de'>Home</a> |");
+        document.write("<a class='styleforbid' title='Coming Soon'>Projects</a> |");
+        document.write("<a class='styleforbid' title='Coming Soon'>RUT</a> |");
+        document.write("<a class='styleforbid' title='Coming Soon'>Extra</a> |");
+        document.write("<a class='headerstyle' href='https://refreshinglemona.de/about'>About</a>");
+    document.write("</div>");
+document.write("</div>");
