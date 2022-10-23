d27e8731e5821ae1ab8ce044aea6f94191a8dbbb
refreshing-lemonade <75630326+refreshing-lemonade@users.noreply.github.com>
2022-10-22 21:29:02 -0700

    Add files via upload

--- global/header.js
+++ global/header.js
@@ -15,13 +15,13 @@ function showSubMenu() {
     }
 }
 
-//    Home | Projects | RUT | Extra | About
+//    Home | Projects | Books | Extra | About
 document.write("<header>");
     document.write("<div style='display:flex; float:right;'>");
         document.write("<img src='https://refreshinglemona.de/global/zap.png' alt='Zap' style='height:90px;'>");
         document.write("<div id='headerbox'>");
             document.write("<a class='headerstyle' href='https://refreshinglemona.de'>Home</a> | ");
-            document.write("<a class='styleforbid' title='Coming Soon'>Projects</a> | ");
+            document.write("<a class='headerstyle' href='https://refreshinglemona.de/projects'>Projects</a> | ");
             document.write("<a class='styleforbid' title='Coming Soon'>Books</a> | ");
             document.write("<a class='styleforbid' title='Coming Soon'>Extra</a> | ");
             document.write("<a class='headerstyle' href='https://refreshinglemona.de/about'>About</a> ");
@@ -34,13 +34,13 @@ document.write("<div class='headersub-container'>");
     document.write("<div id='headersub'><table>");
         document.write("<tr>");
             document.write("<th><a href='https://refreshinglemona.de'>Home</a></th>");
-            document.write("<th class='styleforbid'>Projects</th>");
+            document.write("<th><a href='https://refreshinglemona.de/projects'>Projects</a></th>");
             document.write("<th class='styleforbid'>Books</th>");
             document.write("<th class='styleforbid'>Extra</th>");
             document.write("<th><a href='https://refreshinglemona.de/about'>About</a></th>");
         document.write("</tr><tr>");
             document.write("<td></td>");
-            document.write("<td class='styleforbid'>Rom Hacks</td>");
+            document.write("<td class='styleforbid'>Gallery</td>");
             document.write("<td class='styleforbid'>Oswald, Snow Journey</td>");
             document.write("<td class='styleforbid'>Videos</td>");
             document.write("<td></td>");
@@ -52,7 +52,7 @@ document.write("<div class='headersub-container'>");
             document.write("<td></td>");
         document.write("</tr><tr>");
             document.write("<td></td>");
-            document.write("<td class='styleforbid'>Other</td>");
+            document.write("<td></td>");
             document.write("<td></td>");
             document.write("<td class='styleforbid'>Tournaments</td>");
             document.write("<td></td>");
